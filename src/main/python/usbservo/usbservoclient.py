2f405e7ec19e1cad8815bd6c7eb2af7e284f4a0e
aadebuger <zhvxxh@gmail.com>
2017-07-15 21:45:35 +0800

    first commit

---
+++ src/main/python/usbservo/usbservoclient.py
@@ -0,0 +1,13 @@
+'''
+Created on 2017年7月15日
+
+@author: aadebuger
+'''
+import serial
+def testservo():
+        ser = serial.Serial('/dev/ttyUSB0') 
+        print(ser.name)  
+        ser.write(b'#1P1500T100\r\n')
+        ser.close()
+if __name__ == '__main__':
+     testservo()
